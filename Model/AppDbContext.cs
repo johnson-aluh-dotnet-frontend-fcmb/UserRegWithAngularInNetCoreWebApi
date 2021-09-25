b8ce9f1b154b7436eb504febad86a1c96569d1a6
AluhJohnson <johnsonaluh@gmail.com>
2021-09-25 08:35:26 +0100

    Added AppDbContext Class in Model folder

---
+++ Model/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserRegWithAngularInNetCoreWebApi.Model
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,8 @@ namespace UserRegWithAngularInNetCoreWebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
